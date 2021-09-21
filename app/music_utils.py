8910760c55d93dd6b7dc2a93cf03f388debb9b04
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-09-21 12:31:12 -0300

    Helper for the music commands

---
+++ app/music_utils.py
@@ -0,0 +1,65 @@
+import asyncio
+from config import *
+from app.utils import guild_to_settings
+
+
+async def connect_to_channel(guild, destiny_channel, ctx, switch=False, default=True):
+    """
+    Connects the bot to the specified voice channel.
+        Args:
+            guild: The guild for witch the operation should be performed.
+            switch: Determines if the bot should disconnect from his current channel to switch channels.
+            default: Determines if the bot should default to the first channel, if the name was not found.
+    """
+
+    for channel in guild.voice_channels:
+        if str(channel.name).strip() == str(destiny_channel).strip():
+            if switch:
+                try:
+                    await guild.voice_client.disconnect()
+                except:
+                    await ctx.send(NOT_CONNECTED_MESSAGE)
+            
+            await channel.connect()
+            return
+        
+        if default:
+            try:
+                await guild.voice_channels[0].connect()
+            except:
+                await ctx.send(DEFAULT_CHANNEL_JOIN_FAILED)
+        else:
+            await ctx.send(CHANNEL_NOT_FOUND_MESSAGE + str(destiny_channel))
+
+async def is_connected(ctx):
+    try:
+        voice_channel = ctx.guild.voice_client.channel
+        return voice_channel
+    except:
+        return None
+
+async def play_check(ctx):
+    sett = guild_to_settings[ctx.guild]
+    vc_rule = sett.get('user_must_be_in_vc')
+
+    if vc_rule == True:
+        author_voice = ctx.message.author.voice
+        bot_vc = ctx.guild.voice_client.channel
+        if author_voice == None:
+            await ctx.send(USER_NOT_IN_VC_MESSAGE)
+            return False
+        elif ctx.message.author.voice.channel != bot_vc:
+            await ctx.send(USER_NOT_IN_VC_MESSAGE)
+            return False
+
+class Timer:
+    def __init__(self, callback):
+        self._callback = callback
+        self._task = asyncio.create_task(self._job())
+
+    async def _job(self):
+        await asyncio.sleep(VC_TIMEOUT)
+        await self._callback()
+
+    def cancel(self):
+        self._task.cancel()
