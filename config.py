d0c1021d26bec0c05853e0d6b5b045d9e050acdb
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-09-21 12:28:00 -0300

    A bunch of helpers

---
+++ config.py
@@ -0,0 +1,39 @@
+BOT_PREFIX = "j."
+EMBED_COLOR = 0xf0e130
+
+SUPPORTED_EXTENSIONS = ('.webm', '.mp4', '.mp3', '.avi', '.wav', '.m4v', '.ogg', '.mov')
+
+GLOBAL_DISABLE_AUTOJOIN_VC = False
+
+STARTUP_MESSAGE = "Starting Bot..."
+STARTUP_COMPLETE_MESSAGE = "Startup Complete"
+
+USER_NOT_IN_VC_MESSAGE = "Erro: Entre em um canal de voz para poder usar esse comando."
+NOT_CONNECTED_MESSAGE = "Erro: O Bot não está conectado a nenhum canal de voz"
+CHANNEL_NOT_FOUND_MESSAGE = "Erro: Canal não encontrado"
+DEFAULT_CHANNEL_JOIN_FAILED = "Erro: Não foi possível entrar no canal"
+ALREADY_CONNECTED_MESSAGE = "Erro: O Bot já está conectado a um canal de voz"
+
+INFO_HISTORY_TITLE = "Músicas tocadas:"
+MAX_HISTORY_LENGTH = 10
+MAX_TRACKNAME_HISTORY_LENGTH = 15
+
+MAX_SONG_PRELOAD = 7  #maximum of 25
+
+VC_TIMEOUT = 600 #seconds
+VC_TIMEOUT_DEFAULT = True
+ALLOW_VC_TIMEOUT_EDIT = True  #allow or disallow editing the vc_timeout guild setting
+
+SONGINFO_UPLOADER = "Uploader: "
+SONGINFO_DURATION = "Duração: "
+SONGINFO_SECONDS = "s"
+SONGINFO_LIKES = "Likes: "
+SONGINFO_DISLIKES = "Dislikes: "
+SONGINFO_NOW_PLAYING = "Tocando"
+SONGINFO_QUEUE_ADDED = "Adicionado à fila"
+SONGINFO_SONGINFO = "Informações da música:"
+SONGINFO_UNKNOWN_SITE = "Link desconhecido :question:"
+SONGINFO_PLAYLIST_QUEUED = "Fila: :page_with_curl:"
+SONGINFO_UNKNOWN_DURATION = "Desconhecido"
+
+ABSOLUTE_PATH = '' #do not modify
